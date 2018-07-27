dcccca2f30b2d0f147ed4c2de10636c6f7352327
MaliciousHex <harryermawan@gmail.com>
2018-07-27 22:36:48 +0700

    actions mutations store

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,6 +1,22 @@
 <template>
   <div id="app">
-    Got Jokes?
+    <h3>Got Jokes?</h3>
+    <button class="btn btn-primary" @click="initJokes">Add More Jokes</button>
+    <div>
+
+    </div>
     {{$store.state}}
   </div>
 </template>
+
+<script>
+
+import {mapActions} from 'vuex'
+
+export default {
+  methods: mapActions([
+    'initJokes'
+  ])
+}
+</script>
+
