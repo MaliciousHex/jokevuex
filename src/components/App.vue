13f273b93cda61e985626f7c584916c10daa9fef
MaliciousHex <harryermawan@gmail.com>
2018-08-18 19:17:03 +0700

    finish

--- src/components/App.vue
+++ src/components/App.vue
@@ -2,21 +2,44 @@
   <div id="app">
     <h3>Got Jokes?</h3>
     <button class="btn btn-primary" @click="initJokes">Add More Jokes</button>
-    <div>
-
+    <button class="btn btn-primary" @click="addJoke">Add a Joke</button>
+    <br>
+    <span v-for="type in types" :key="type">
+      <input type="checkbox" :value="type" v-model="checkedTypes" checked>
+      <label for="checkbox">{{type}}</label>&nbsp;
+    </span>
+    <br>
+    <div class="col-md-12">
+      <Joke
+        v-for="(joke,index) in $store.state.jokes"
+        v-show="checkedTypes.includes(joke.type)"
+        :joke="joke"
+        :index="index"
+        :key="index"/>
     </div>
-    {{$store.state}}
+
   </div>
 </template>
 
 <script>
 
 import {mapActions} from 'vuex'
+import Joke from './Joke.vue'
 
 export default {
+  data(){
+    return{
+      types: ['general' , 'knock-knock','programming'],
+      checkedTypes: ['general','knock-knock','programming']
+    }
+  },
   methods: mapActions([
-    'initJokes'
-  ])
+    'initJokes',
+    'addJoke'
+  ]),
+  components:{
+    Joke
+  }
 }
 </script>
 
